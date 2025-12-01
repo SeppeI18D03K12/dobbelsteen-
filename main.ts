b3bd2051c23e955adfbccb56e28562bf58cdef8b
SeppeI18D03K12 <seppe.luyten18@gmail.com>
2025-12-01 14:17:26 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(0, 7))
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
